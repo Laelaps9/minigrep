713e207949dfe186bfb0d2a0a18fb26025d1123b
Laelaps9 <laelaps9@tutanota.com>
2021-11-16 19:24:22 -0600

    Add functionality to accept command line arguments and read file Program now takes 2 command line arguments which are interpreted as the query and the filename to be read. The program also reads the file properly and prints the contents.

---
+++ target/debug/deps/minigrep-9e24cbddab9da2e0.d
@@ -0,0 +1,5 @@
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-9e24cbddab9da2e0: src/main.rs
+
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-9e24cbddab9da2e0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/laelaps/Practice/Rust/minigrep/target/debug/minigrep: /home/laelaps/Practice/Rust/minigrep/src/main.rs
