0f6e004dfb2db3ba074b08b003b2a3da5f009588
Laelaps9 <laelaps9@tutanota.com>
2021-11-18 17:24:02 -0600

    Add query search to program Program now searches for the query given in the text file and returns every line containing that word. A simple test was created to ensure that the search function returns the correct lines.

---
+++ target/debug/deps/minigrep-85fc03e9802fdf1e.d
@@ -0,0 +1,5 @@
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-85fc03e9802fdf1e: src/main.rs
+
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-85fc03e9802fdf1e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/minigrep-cfa2da76a6158a14.d
@@ -0,0 +1,5 @@
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-cfa2da76a6158a14: src/lib.rs
+
+/home/laelaps/Practice/Rust/minigrep/target/debug/deps/minigrep-cfa2da76a6158a14.d: src/lib.rs
+
+src/lib.rs:
